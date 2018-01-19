1422e0863231a19ea719f8c384fde04c80d2b5a9
landongw <landon@extantpublishing.com>
2018-01-19 12:34:10 -0600

    change location of upload directory

--- src/main/java/com/wiedenman/b_plate/config/StorageProperties.java
+++ src/main/java/com/wiedenman/b_plate/config/StorageProperties.java
@@ -10,7 +10,7 @@ public class StorageProperties {
     /**
      * Folder location for storing files
      */
-    private String location = "upload-dir";
+    private String location = "src/main/resources/static/assets/images";
 
     public String getLocation() {
         return location;
